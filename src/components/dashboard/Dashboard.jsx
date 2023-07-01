407d40015e06f363714ed82c52ed5296c30e6dc7
unknown <kasmi_b@etna-alternance.net>
2023-07-01 17:48:33 +0200

    dashboard updates

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -7,42 +7,57 @@ const Dashboard = () => {
   return (
     <div className="dashboard">
       <div className="column">
-        <div className="column-header orange-bg"> 
+        <div className="column-header">
           <h2>Emargements non faits</h2> 
         </div>
-        <h1>Jeudi 15 Dec 2022</h1> 
-        <div className="card white-bg">
+        <h1 className='date'>Jeudi 15 Dec 2022</h1> 
+        <div className="card">
           <h3>Sciences islamiques 2ème année</h3>
-          <p>Jeu 18h00 à 12h00</p> 
+          <div className='row'>
+          <p className='day'>Jeu 18h00 à 12h00</p>
+          <p className='session'>11/32</p>
+          </div> 
         </div>
       </div>
       <div className="column">
         <div className="column-header orange-bg">
           <h2>Mes emargements</h2>
         </div>
-        <h1>Samedi 17 Dec 2022</h1>
-        <div className="card white-bg">
+        <h1 className='date'>Samedi 17 Dec 2022</h1>
+        <div className="card">
           <h3>Sciences islamiques 2ème année</h3>
-          <p>Jeu 18h00 à 12h00</p>
+          <div className='row'>
+          <p className='day'>Jeu 18h00 à 12h00</p>
+          <p className='session'>11/32</p>
+          </div>
         </div>
       </div>
       <div className="column">
         <div className="column-header orange-bg">
           <h2>Mon agenda</h2>
         </div>
-        <h1>Samedi 17 Dec 2022</h1>
-        <div className="card white-bg">
+        <h1 className='date'>Samedi 17 Dec 2022</h1>
+        <div className="card">
           <h3>Sciences islamiques 2ème année</h3>
-          <p>Jeu 18h00 à 12h00</p>
+          <div className='row'>
+          <p className='day'>Jeu 18h00 à 12h00</p>
+          <p className='session'>11/32</p>
+          </div>
         </div>
-        <div className="card white-bg">
+        <div className="card">
           <h3>Sciences islamiques 1ére année</h3>
-          <p>Jeu 18h00 à 12h00</p>
+          <div className='row'>
+          <p className='day'>Jeu 18h00 à 12h00</p>
+          <p className='session'>11/32</p>
+          </div>
         </div>
-        <h1>Samedi 19 Dec 2022</h1>
+        <h1 className='date'>Samedi 19 Dec 2022</h1>
         <div className="card white-bg">
           <h3>Sciences islamiques 2ème année</h3>
-          <p>Jeu 18h00 à 12h00</p>
+          <div className='row'>
+          <p className='day'>Jeu 18h00 à 12h00</p>
+          <p className='session'>11/32</p>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar'
 import Sidebar from '../components/sidebar/Sidebar';
+import Dashboard from '../components/dashboard/Dashboard';
 
 const DashboardPage = () => {
   return (
     <div>
     <Navbar />
     <Sidebar />
+    <Dashboard />
     </div>
 );
 };
