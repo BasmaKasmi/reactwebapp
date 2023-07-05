4385514db467eda670de415c72206ce7cf550f5e
unknown <kasmi_b@etna-alternance.net>
2023-07-05 05:57:23 +0200

    login page finish

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,13 +2,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
-import { FaUserCircle } from 'react-icons/fa';
-import { BiLock } from 'react-icons/bi';
 
 // Déclaration du composant fonctionnel Login
 const Login = () => {
   return (
-    <center className="safe-area">
+    <div className="safe-area">
       <div className="login-container">
       {/* Titre de la page */}
         <h1 className="title">Espace Professeur</h1>
@@ -16,12 +14,10 @@ const Login = () => {
         <p className="subtitle">Connectez-vous et notez l'assiduité de vos groupes</p>
         {/* Champ d'entrée pour l'identifiant */}
         <div className="input-container">
-          <FaUserCircle className="icon alternate-email-icon"/>
           <input className="input-field" type="text" placeholder="Identifiant" />
         </div>
         {/* Champ d'entrée pour le mot de passe */}
         <div className="input-container">
-          <BiLock className="icon lock-icon"/>
           <input className="input-field" type="password" placeholder="Password" />
           {/* Bouton pour mot de passe oublié */}
         </div>
@@ -32,7 +28,7 @@ const Login = () => {
           <img className="image" src={require('../../assets/logo shatibi.png')} alt="ShatibiPic" />
         </div>
       </div>
-    </center>
+    </div>
   );
 };
 
