f3de352a3d5383369141959a078080741a7c37f3
unknown <kasmi_b@etna-alternance.net>
2023-07-21 06:59:28 +0200

    emargementsNf desktop

--- src/pages/AgendaPage.js
+++ src/pages/AgendaPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar'
 import Sidebar from '../components/sidebar/Sidebar';
+import Agenda from '../components/agenda/Agenda';
 
 const AgendaPage = () => {
   return (
     <div>
     <Navbar />
     <Sidebar />
+    <Agenda />
     </div>
 );
 };
