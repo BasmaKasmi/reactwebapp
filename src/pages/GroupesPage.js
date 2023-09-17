3f6f9c7f3aafe1adf1ef731984bd7ffc957fa378
Basma <macbook@Basmas-macbook.local>
2023-09-17 05:46:08 +0200

    cards

--- src/pages/GroupesPage.js
+++ src/pages/GroupesPage.js
@@ -1,5 +1,5 @@
 // Importation des modules nécessaires
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/navbar/Navbar';
 import Sidebar from '../components/sidebar/Sidebar';
 import RetourButton from '../components/retourbutton/RetourButton';
@@ -11,6 +11,12 @@ import ColonneGroupe2 from '../components/colonnegroupe2/ColonneGroupe2';
 
 // Déclaration du composant GroupesPage
 const GroupesPage = () => {
+  const [selectedCard, setSelectedCard] = useState({ title: '', date: '' });
+
+  const handleCardClick = (title, date) => {
+    setSelectedCard({ title, date });
+  };
+  
   return (
     <div>
       <div className="nav-emarg-container">
@@ -32,10 +38,10 @@ const GroupesPage = () => {
         <Sidebar />
         </div>
         <div className="column" style={{ flexBasis: '25%', margin:'10px', boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.12)'  }}>
-          <ColonneGroupes />
+        <ColonneGroupes onCardClick={handleCardClick} />
         </div>
         <div className="column" style={{ flexBasis: '50%', margin:'10px', boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.12)'}}>
-          <ColonneGroupe2 />
+          <ColonneGroupe2 selectedCard={selectedCard} />   
         </div>
       </div>
       </div>
